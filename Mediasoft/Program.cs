788a9641d972d04d4c3a9264630fae47b5e32316
Vovenskyi <vovenskyi@gmail.com>
2023-06-01 21:00:36 +0400

    change of variables

--- Mediasoft/Program.cs
+++ Mediasoft/Program.cs
@@ -10,15 +10,15 @@ namespace Mediasoft
             {
                 if (j % 3 == 0 && j % 5 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
+                    Console.WriteLine("FB");
                 }
                 else if (j % 3 == 0)
                 {
-                    Console.WriteLine("Fizzik");
+                    Console.WriteLine("F");
                 }
                 else if (j % 5 == 0)
                 {
-                    Console.WriteLine("Buzz");
+                    Console.WriteLine("B");
                 }
                 else
                 {
